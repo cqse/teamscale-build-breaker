2a92b83ff6333a5cf0a4149a045dd6915bb9bb09
Thomas Pettinger <pettinger@cqse.eu>
2025-06-17 11:34:06 +0200

    TS-32080 Add TODOs

--- src/main/java/com/teamscale/buildbreaker/evaluation/Finding.java
+++ src/main/java/com/teamscale/buildbreaker/evaluation/Finding.java
@@ -1,5 +1,8 @@
 package com.teamscale.buildbreaker.evaluation;
 
+/**
+ * TODO
+ */
 public class Finding {
     public final String id;
     public final String group;
@@ -8,6 +11,16 @@ public class Finding {
     public final String uniformPath;
     public final ProblemCategory assessment;
 
+    /**
+     * TODO
+     *
+     * @param id
+     * @param group
+     * @param category
+     * @param message
+     * @param uniformPath
+     * @param assessment
+     */
     public Finding(String id, String group, String category, String message, String uniformPath, ProblemCategory assessment) {
         this.id = id;
         this.group = group;
--- src/main/java/com/teamscale/buildbreaker/evaluation/MetricViolation.java
+++ src/main/java/com/teamscale/buildbreaker/evaluation/MetricViolation.java
@@ -1,5 +1,8 @@
 package com.teamscale.buildbreaker.evaluation;
 
+/**
+ * TODO
+ */
 public class MetricViolation {
     public final String displayName;
     public final String yellowThreshold;
@@ -7,6 +10,15 @@ public class MetricViolation {
     public final String formattedTextValue;
     public final ProblemCategory rating;
 
+    /**
+     * TODO
+     *
+     * @param displayName
+     * @param yellowThreshold
+     * @param redThreshold
+     * @param formattedTextValue
+     * @param rating
+     */
     public MetricViolation(String displayName, String yellowThreshold, String redThreshold, String formattedTextValue, ProblemCategory rating) {
         this.displayName = displayName;
         this.yellowThreshold = yellowThreshold;
