4e2eccf294b199e94154025e9ad97258a173fe69
Thomas Pettinger <pettinger@cqse.eu>
2025-06-17 11:01:38 +0200

    TS-32080 Remove catching class cast exception from FindingsEvaluator

--- src/main/java/com/teamscale/buildbreaker/evaluation/FindingsEvaluator.java
+++ src/main/java/com/teamscale/buildbreaker/evaluation/FindingsEvaluator.java
@@ -1,20 +1,12 @@
 package com.teamscale.buildbreaker.evaluation;
 
-import com.teamscale.buildbreaker.exceptions.BuildBreakerInternalException;
 import org.conqat.lib.commons.collections.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Evaluates a finding JSON string.
- * <p>
- * This class can handle two different response formats:
- * <ul>
- *   <li>FindingChurnList - from the finding-churn/list endpoint (for single commit evaluation)</li>
- *   <li>ExtendedFindingDelta - from the findings/delta endpoint (for branch comparison)</li>
- * </ul>
- * The class automatically detects which format is being used and processes it accordingly.
+ * TODO
  */
 public class FindingsEvaluator {
 
@@ -26,17 +18,11 @@ public class FindingsEvaluator {
             findingsToEvaluate.addAll(findings.getSecond());
         }
 
-        try {
-            for (Finding finding : findingsToEvaluate) {
-                if (finding.assessment == ProblemCategory.WARNING && !failOnYellowFindings) {
-                    continue;
-                }
-                evaluationResult.addViolation(finding.assessment, finding.toString());
+        for (Finding finding : findingsToEvaluate) {
+            if (finding.assessment == ProblemCategory.WARNING && !failOnYellowFindings) {
+                continue;
             }
-        } catch (ClassCastException e) {
-            throw new BuildBreakerInternalException(
-                    "Could not parse JSON response:\n" + findings + "\n\nPlease contact CQSE with an error report.",
-                    e);
+            evaluationResult.addViolation(finding.assessment, finding.toString());
         }
         return evaluationResult;
     }
